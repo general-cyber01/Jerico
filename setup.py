353eae7a6bf4db7f9b91b343934cc62792cdef6e
general-cyber01 <56541695+general-cyber01@users.noreply.github.com>
2026-02-11 11:17:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="jerico",
-    version="0.1",
+    version="1.1",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
-            "jerico=jerico.main:main"
-        ]
+            "jerico=jerico.main:main",
+        ],
     },
+    install_requires=[
+        "psutil",
+    ],
+    python_requires='>=3.10',
 )
-
